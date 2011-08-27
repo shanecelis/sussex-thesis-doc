6572b424e79a840149b4f3f759ee9b0c4ce4aa67
Shane Celis <shane@gnufoo.org>
2011-08-27 19:56:33 -0400

    Fixing up the thesis some more.

--- src/make-frog_eqns_dotsolved.m
+++ src/make-frog_eqns_dotsolved.m
@@ -1,5 +1,5 @@
 SetDirectory["/Users/shane/School/sussex/thesis/code/src"];
-Import["frog_eqns.m"]; eqns = Map[# == 0&, rhseqns]; 
-eqns = solveEqnsForDotVars[eqns, {u1[t], u2[t], u3[t], u4[t], u5[t], u6[t], u7[t], u8[t]}, t];
+Import["frog_eqns.m"]; eqns0 = Map[# == 0&, rhseqns]; 
+eqns = solveEqnsForDotVars[eqns0, {u1[t], u2[t], u3[t], u4[t], u5[t], u6[t], u7[t], u8[t]}, t];
 Export["frog_eqns_dotsolved.m", eqns]
 ResetDirectory[];
